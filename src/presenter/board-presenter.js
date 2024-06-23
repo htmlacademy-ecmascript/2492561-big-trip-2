605454c6830584459a6892bb8eada6f6edb1f53f
Toriboba <geor.chudin@gmail.com>
2024-06-23 05:04:42 +0300

    Правки

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,10 +1,8 @@
 import TripInfo from '../view/trip-info';
 import TripEventsSort from '../view/trip-events-sort';
 import TripEventsList from '../view/trip-events-list';
-import EventListItem from '../view/trip-list-item';
 import TripEvent from '../view/trip-event';
 import EventEdit from '../view/event-edit';
-import EventDetails from '../view/event-details';
 import EventOffers from '../view/event-offers';
 import EventDestination from '../view/event-destination';
 import { render } from '../render';
@@ -13,9 +11,6 @@ export default class BoardPresenter {
   tripControlsContainer = document.querySelector('.trip-main');
   tripEventsContainer = document.querySelector('.trip-events');
   tripEventList = new TripEventsList();
-  eventListItem = new EventListItem();
-  eventEdit = new EventEdit();
-  eventDetails = new EventDetails();
 
 
   initMain() {
@@ -25,11 +20,10 @@ export default class BoardPresenter {
     //Рендер нового ивента
     render(new TripEventsSort(), this.tripEventsContainer);
     render(this.tripEventList,this.tripEventsContainer);
-    render (this.eventListItem, this.tripEventList.getElement());
-    render(this.eventEdit, this.eventListItem.getElement());
-    render(this.eventDetails, this.eventEdit.getElement());
-    render(new EventOffers(), this.eventDetails.getElement());
-    render(new EventDestination(), this.eventDetails.getElement());
+    render(new EventEdit(), this.tripEventList.getElement());
+    const eventDetails = document.querySelector('.event__details');
+    render(new EventOffers(), eventDetails);
+    render(new EventDestination(), eventDetails);
 
     //Рендер уже созданных ивентов
     for (let i = 0; i < 3; i++) {
